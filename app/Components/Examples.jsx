27bff25d413d9b6624a489736574ae5b623577e8
Lorenzo Rheinicke <lorenzorheinicke@gmail.com>
2016-07-18 00:02:10 +0200

    Added copy

--- app/Components/Examples.jsx
+++ app/Components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('React');
 var Examples = React.createClass({
   render: function(){
     return (
-      <h3>Examples Page</h3>
+      <div>
+        <h3>Examples Page</h3>
+        <p>Welcome to examples page.</p>
+      </div>
     );
   }
 });
