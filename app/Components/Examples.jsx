ffbe8f748bc85865436ffa2adc7336b7721442f5
Lorenzo Rheinicke <lorenzorheinicke@gmail.com>
2016-07-20 21:25:44 +0200

    Style Main and Navigation

--- app/Components/Examples.jsx
+++ app/Components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('React');
+var {Link} = require('react-router');
 
 var Examples = React.createClass({
   render: function(){
     return (
       <div>
-        <h3>Examples Page</h3>
-        <p>Welcome to examples page.</p>
+        <h1 className="text-center">Examples Page</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to="/?location=Bredasdorp">Bredasdorp</Link>
+          </li>
+          <li>
+            <Link to="/?location=Stellenbosch">Stellenbosch</Link>
+          </li>
+        </ol>
       </div>
     );
   }
